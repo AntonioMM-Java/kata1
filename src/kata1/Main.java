410e022fbe89e383478618c619cbc2a5aed50d98
Usuario <Usuario@DESKTOP-DGNAOIQ>
2020-12-26 19:45:10 +0000

    Terminado 1 6 mins

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata1;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
- *
- * @author Usuario
+ * @author Antonio Miguel Martel 
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hola soy la kata1");
+        
+        Person persona1 = new Person("ANtonio Martel", LocalDate.of(2000, Month.DECEMBER, 16));
+        System.out.println(persona1.getName() + " " + persona1.getAge());
     }
     
 }
